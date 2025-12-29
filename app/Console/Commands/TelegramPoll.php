2b6a38e5b6797fba578aa47975059122f590e407
aditya <adityarifkistemba@gmail.com>
2025-12-29 16:43:56 +0700

    fix : add telepoll

---
+++ app/Console/Commands/TelegramPoll.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TelegramPoll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:telegram-poll';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+          $token = config('services.telegram.bot_token');
+        $offsetFile = storage_path('app/telegram_offset.txt');
+
+        $offset = file_exists($offsetFile)
+            ? (int) file_get_contents($offsetFile)
+            : 0;
+
+        $response = Http::timeout(30)->get(
+            "https://api.telegram.org/bot{$token}/getUpdates",
+            [
+                'timeout' => 25,
+                'offset'  => $offset + 1,
+            ]
+        );
+
+        if (!$response->ok()) {
+            $this->error('Telegram API error');
+            return;
+        }
+
+        $updates = $response->json('result', []);
+
+        foreach ($updates as $update) {
+            file_put_contents($offsetFile, $update['update_id']);
+
+            // 👉 PROSES PESAN DI SINI
+            logger()->info('Telegram update', $update);
+        }
+    }
+}
